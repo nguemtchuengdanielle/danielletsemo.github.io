a6820ea0cb4977a11f4e8499f034f0b0f2df73d5
nguemtchuengdanielle <nguemtchueng.t.danielle@aims-senegal.org>
2026-05-03 19:31:27 +0000

    new style

--- assets/js/script.js
+++ assets/js/script.js
@@ -247,3 +247,15 @@ makeSlider('#certifications .cert-slider', '#certifications .cert-prev', '#certi
 
 // Conferences slider
 makeSlider('#conferences .conf-slider', '#conferences .conf-prev', '#conferences .conf-next', '#conferences .conf-dots', 3500);
+
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/69f7714d0b9cc71c3209418e/1jnn956cp';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+// End of Tawk.to Live Chat

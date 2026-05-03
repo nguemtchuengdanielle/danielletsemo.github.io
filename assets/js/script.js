bb38ec917ca929003d534c497e033853c4bc3cae
nguemtchuengdanielle <nguemtchueng.t.danielle@aims-senegal.org>
2026-05-03 16:57:59 +0000

    New portfolio -new indexe

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,7 +200,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/69f5e6440f7c9c1c2fb07e1e/1jnk8mv3b';
+    s1.src = 'https://embed.tawk.to/69f7714d0b9cc71c3209418e/1jnn956cp';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
